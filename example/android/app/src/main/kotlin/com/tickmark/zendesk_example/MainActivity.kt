3a989be57f9695b1f1870116806c5181016904d1
Raphael Pontes <raphaelkennedy@hotmail.com>
2021-12-13 18:23:32 -0300

    fork from https://github.com/taxfyle

---
+++ example/android/app/src/main/kotlin/com/tickmark/zendesk_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tickmark.zendesk_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:zendesk_plugin/zendesk_plugin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initZendesk();
+  }
+
+  Future<void> initZendesk() async {
+    if (!mounted) {
+      return;
+    }
+    await Zendesk.initialize('', '');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Zendesk Chat Plugin'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'Initialize  example with proper\nkeys in main.dart',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              MaterialButton(
+                onPressed: openChat,
+                color: Colors.blueGrey,
+                textColor: Colors.white,
+                child: const Text('Open Chat'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> openChat() async {
+    try {
+      await Zendesk.setVisitorInfo(
+          name: 'Text Client',
+          email: 'test+client@example.com',
+          phoneNumber: '0000000000',
+          department: 'Support');
+      await Zendesk.startChat(primaryColor: Colors.red);
+    } on dynamic catch (ex) {
+      print('An error occured $ex');
+    }
+  }
+}
